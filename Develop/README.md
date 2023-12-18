0c714211ba1a4fabe7c98a0377a1702c58c54809
Tina Humphrey <humphrey14t@gmail.com>
2023-12-18 18:31:56 -0500

    commit javascript changes

--- Develop/README.md
+++ Develop/README.md
@@ -1,9 +1,33 @@
-# README
-  ## undefined
-  ## README
-  ## README
-  ## README
-  ## README
-  ## README
-  ## README
-  ## README
+# README Generator
+
+  ## Table of Contents
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribute](#Contribute)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Contact Me](#Contact Me)
+  
+  ## Description
+  an application using node to create a README file
+
+  ## Installation
+  these are the steps
+
+  ## Usage
+  these are the instructions
+
+  ## Contribute
+  this is the application
+
+  ## Tests
+   these are the tests
+
+  ## License 
+  ![license](http://img.shields.io/badge/license-MIT-informational.svg?style=for-the-badge&logo=appveyor)
+  This project uses a MIT license.
+  
+  ## Contact Me
+   humphreyt12
+   humphrey14t@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,19 +10,45 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.name}
-  ## ${data.name.description}
-  ## ${data.name}
-  ## ${data.name}
-  ## ${data.name}
-  ## ${data.name}
-  ## ${data.name}
-  ## ${data.name}
-  ## ${data.name}
+
+  ## Table of Contents
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribute](#Contribute)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Contact Me](#Contact Me)
+  
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribute
+  ${data.contribute}
+
+  ## Tests
+   ${data.tests}
+
+  ## License 
+  ![license](http://img.shields.io/badge/license-${data.license}-informational.svg?style=for-the-badge&logo=appveyor)
+  This project uses a ${data.license} license.
+
+  ## Contact Me
+  Github: ${data.github}
+  Email: ${data.email}
 `;
 }
 
